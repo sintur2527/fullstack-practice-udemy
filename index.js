bfb2f2bea95861d4b7450851d04856202c905041
Soumith Inturi <sintur2527@gmail.com>
2019-06-23 00:32:29 -0400

    Setup Google OAuth

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'soumith' });
-});
+// creates new instance of google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // we can now take the user info that we get back from google and save it to our database
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+// when user goes to this route, we want to kick them off into the OAuth flow
+// GoogleStrategy has internal identifier of 'google'
+// scope specifies what access we want to have inside of google
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+// this time google strategy will see the code in the url
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
